da5453f68659b64590c214e0bd3ad3d3179f756b
smoroso <stephane.moroso@gmail.com>
2017-09-08 16:50:18 +0200

    ongoing

--- public/service-worker/with/index.js
+++ public/service-worker/with/index.js
@@ -1,8 +1,25 @@
 "use strict";
 
-((document) => {
-  const buttonEl = document.getElementById("trigger-code");
-  buttonEl.addEventListener("click", async () => {
-    alert("LOL");
-  });
-})(document);
+((window, document, navigator) => {
+  const nextPageBtnEl = document.getElementById("next-page-btn");
+  const previousPageBtnEl = document.getElementById("previous-page-btn");
+  const pageNumberEl = document.getElementById("page-number");
+  const timeContainer = document.getElementById("time-container");
+  const timeEl = document.getElementById("time");
+  const loadingSpinnerContainer = document.getElementById("loading-spinner-container");
+  const displayResultEl = document.getElementById("display-result");
+  const imageBaseUrl = "https://unsplash.it/458/354?image=";
+  const totalImages = 36;
+  const minPage = 1;
+  const maxPage = 3;
+
+  let pageNumber;
+
+  const initialize = () => {
+    console.log(navigator);
+  };
+
+  ////////////////////
+
+  initialize();
+})(window, document, navigator);
